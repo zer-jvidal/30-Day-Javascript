5279e523d14c2a4a8fbd9698e8a450f9792425ba
JE Vidal <je.kunvidal@gmail.com>
2026-01-04 14:14:19 +0800

    Combining Names in Javascript

---
+++ Day 4: Combining Names/NameCombination.js
@@ -0,0 +1,12 @@
+// Day 4 Challenge : Name Combination
+
+let firstName = "Kyoya";
+let lastName = "Hibari";
+
+let masterMe = "J.E.";
+let masterMeLast = "Vidal";
+
+let fullName = firstName + " " + lastName;
+let masterFullName = masterMe + " " + masterMeLast;
+
+console.log(masterFullName);
