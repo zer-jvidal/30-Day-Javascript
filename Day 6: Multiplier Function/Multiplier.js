f5dc54995319d2d5790fc917d374501c18ebd4a6
JE Vidal <je.kunvidal@gmail.com>
2026-01-08 22:26:18 +0800

    Multiplier Function in Javascript

---
+++ Day 6: Multiplier Function/Multiplier.js
@@ -0,0 +1,8 @@
+// Day 6 of Javascript : Multiplier Function
+
+function multiply(x, y) {
+    return x * y;
+}
+
+let result = multiply(7, 3);
+console.log("Here is the result my master: " + result);
