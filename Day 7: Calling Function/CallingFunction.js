b2470fa7175596351ef7b3cbd965d08f39155a31
JE Vidal <je.kunvidal@gmail.com>
2026-01-09 22:30:14 +0800

    Calling Function - Day 7 of my JS Challenge

---
+++ Day 7: Calling Function/CallingFunction.js
@@ -0,0 +1,24 @@
+// DAy 7 of JS Challenge. Calling a Function
+
+function multiply(x,y) {
+    return x *y;
+}
+
+//Variables
+x = 7;
+y = 3;
+
+// Direct Log
+console.log("Here is the result folks: " + multiply(3,7));
+
+// Store then log
+let result = multiply(x,y);
+console.log("The result is: " + result);
+
+// Second Variables
+a = "JE";
+b = "Vidal";
+
+let fullName = a + " " + b;
+
+console.log("My Master's name is: " + fullName);
