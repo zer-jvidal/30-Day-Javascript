6d1dd3f739fd81a02c51646b7ba94da85fc844a6
JE Vidal <je.kunvidal@gmail.com>
2026-01-10 08:49:00 +0800

    Day 8 and 9 of Javascript Challenge
    Array Manipulation and Iteration

---
+++ Day 8: Array/Array Manipulation and Iteration.js
@@ -0,0 +1,19 @@
+// Day 8 of the Challenge. Let's call some fruits! Let's go! <3
+//This covers the Day 9 challenge as well!
+
+// My favorite fruits are:
+let fruits = ["Apple", "Grapes", "Orange", "Pineapple", "Peach"];
+
+console.log(fruits);
+
+console.log(fruits[0]); // apple! <3
+
+console.log("My favorite fruit is " + fruits[0] + "!");
+
+console.log("Hayley's hair color is " + fruits[2] + "!");
+
+function myFunction() {
+    for (let i = 0; i <fruits.length; i++) {
+        console.log(fruits[i]);
+    }
+}
