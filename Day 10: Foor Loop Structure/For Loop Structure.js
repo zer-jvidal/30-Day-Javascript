d92f3d9f5a7a38f45d9cd2ceacd465b04a547946
JE Vidal <je.kunvidal@gmail.com>
2026-01-12 14:40:24 +0800

    For Loop Structure
    For Loop Structure in Javascript

---
+++ Day 10: Foor Loop Structure/For Loop Structure.js
@@ -0,0 +1,10 @@
+// This is Day 10 of the Challenge. Let's loop through some fruits! <3
+
+// My favorite fruits are:
+let fruits = ["Apple", "Grapes", "Orange", "Pineapple", "Peach"];
+
+function myFunction() {
+    for (let i = 0; i <fruits.length; i++) {
+        console.log(fruits[i]);
+    }
+}
