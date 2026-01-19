d2610be96f223364031b92e446e42f632bc37e90
JE Buenaventura <je.kunvidal@gmail.com>
2026-01-19 17:20:36 +0800

    addEventListener
    Day 14 of Javascript practice.

---
+++ Day 14: addEventListener/script.js
@@ -0,0 +1,13 @@
+function changeColor() {
+    document.getElementById("myMaster").style.backgroundColor = "#EBB866";
+}
+
+const btn = document.getElementById("myMeister");
+btn.addEventListener("click", function() {
+    console.log("I got a message for you, Master.");
+});
+
+const btnn = document.getElementById("myMeisterr");
+btnn.addEventListener("click", function() {
+    console.log("Another message for you, Master."); // Use alert instead of console.log if you want a popup! :)
+});
