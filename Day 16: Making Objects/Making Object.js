a44785c40f83ff19c2c00748cc20bc0459b13420
JE Buenaventura <je.kunvidal@gmail.com>
2026-01-21 21:10:06 +0800

    Day 16 of Javascript
    Making Object

---
+++ Day 16: Making Objects/Making Object.js
@@ -0,0 +1,29 @@
+// Day 16 of Javascript! Making Objects.
+// The -ish way to create an object.
+
+let master = {
+    name : "J.E. B. Vidal",
+    monicker : "The -ish!",
+    age : 26,
+    country : "Philippines"
+}
+
+//Rationale above: 'master' is the name of the object. (Variable which is changeable to suit ones preference)
+//Rationale below: 'name', 'monicker', 'age', and 'country' are called properties of the object 'master'.
+
+//Accessing the properties of the object 'master' using dot notation.
+
+console.log(master.name);
+console.log(master.monicker);
+console.log(master.age);
+console.log(master.country);
+
+// Rationale below : Use "Name: ", "Monicker: ", "Age: ", and "Country: " to label the output for better readability.
+
+console.log("Name: ",master.name);
+console.log("Monicker: ",master.monicker);
+console.log("Age: ",master.age);
+console.log("Country: ",master.country);
+
+
+// End of Day 16 of Javascript! Making Objects.
