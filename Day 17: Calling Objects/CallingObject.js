88ede1d35b74f0dab404277981c51e0be9670073
JE Buenaventura <je.kunvidal@gmail.com>
2026-01-22 21:42:23 +0800

    Calling Objects in Javascript
    Day 17 of Javascript Practice

---
+++ Day 17: Calling Objects/CallingObject.js
@@ -0,0 +1,16 @@
+// Day 17 of Javascript Challenge! Summoning the property of the object.
+// Esskeetit! ;)
+
+let master = {
+    name: "J.E. B. Vidal",
+    age: 27,
+    moniker: "The -ish!",
+    country: "Philippines"
+};
+
+console.log(master.age);
+console.log(master.moniker);
+console.log(master.name);
+
+// Has the same approach as day 16. The only difference is that Day 16 should only make an object.
+// But knowing I'm the -ish, I did not only make an object, but also called it's property. Thus covering Day 17, unknowingly.
