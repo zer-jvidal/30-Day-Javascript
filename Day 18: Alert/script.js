5ce49ab87506d456e608196c9d3a4d9e0e223d88
JE Buenaventura <je.kunvidal@gmail.com>
2026-01-24 18:51:18 +0800

    Javascript Alert
    Day 18 of Javascript practice.

---
+++ Day 18: Alert/script.js
@@ -0,0 +1,10 @@
+// Day 18 of Javascript!
+// Summoning someone's name.
+// Let's get it! 
+
+let thyName = prompt("What is thy name?");
+
+alert("Welcome, " + thyName + "! Heaven awaits for your arrival.");
+
+// End of Day 18.
+// Day 19 challenge awaits for me in heaven.
