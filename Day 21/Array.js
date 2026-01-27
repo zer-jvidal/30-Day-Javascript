f1461423d2ca395d66765d4da95f4643985e2e9e
JE Buenaventura <je.kunvidal@gmail.com>
2026-01-27 20:11:07 +0800

    Day 21 of Javascript
    Array

---
+++ Day 21/Array.js
@@ -0,0 +1,16 @@
+// Day 21 of Javascript! 
+// Arrays.
+// Genshin Impact Reference: The Raiden Shogun
+
+let treasure = []; // Items will be stored in the "treasure" array.
+
+// Let's put some items on the treasure chest.
+
+treasure.push("Engulfing Lightning");
+treasure.push("Emblem of Severed Fate");
+treasure.push("Naginata");
+treasure.push("Musou no Hitotachi");
+
+// Let's reveal the chest! 
+
+console.log("Raiden Shogun's Signature Weapon" + ": " + treasure[0]);
