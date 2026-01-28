a40c830d669604a9e0bf60bdf2eacee228f23bcb
JE Buenaventura <je.kunvidal@gmail.com>
2026-01-28 17:31:10 +0800

    Function Parameter
    Day 22 of Javascript

---
+++ Day 22; Function Parameter/FunctionParameter.js
@@ -0,0 +1,11 @@
+// Day 22 of Javascript Challenge! 
+// Function Parameters!
+
+function showLikes(name, item) {
+    console.log(name + " likes " + item);
+}
+
+// functions stored in variables! 
+showLikes("The -ish", "Greatness");
+showLikes("The -ish", "Mastery");
+showLikes("The -ish", "Power");
