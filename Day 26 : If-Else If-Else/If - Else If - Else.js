0ca7262ce2646e1c410138c5c48fc7352b74d96e
JE Buenaventura <je.kunvidal@gmail.com>
2026-02-01 21:09:57 +0800

    Day 26 of Javascript

---
+++ Day 26 : If-Else If-Else/If - Else If - Else.js
@@ -0,0 +1,12 @@
+//  Day 26 of Javascript Challenge
+// IEIE - If Else If - Else
+
+let number = 21;
+
+if (number <= 5) {
+    console.log("Small"); // if 21 is less than or equal to 5; this will be the output.
+} else if (number <= 10) {
+    console.log("Medium"); // if 21 is less than or equal to 10; this will be the output.
+} else {
+    console.log("Large"); // if 21 is greater than 10; this will be the output.
+}
