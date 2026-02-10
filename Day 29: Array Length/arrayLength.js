84865baca51a2169da22054db007520e1d88932d
JE Buenaventura <je.kunvidal@gmail.com>
2026-02-10 19:21:30 +0800

    Day 29 of Javascript Practice
    Array Length

---
+++ Day 29: Array Length/arrayLength.js
@@ -0,0 +1,6 @@
+// Day 29 of Javascript Challenge
+// Array Length
+
+// Log how many items are inside the array.
+let myStuff = ["Steam Deck", "PS5", "PC", "Asus ROG Ally X", "iPhone 14 Pro Max"];
+console.log("Total items: " +myStuff.length); // Output: 5
